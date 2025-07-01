fff5ae8635566966dcbb3156d6665e597a5e35ef
diablo0olbaid <104808307+diablo0olbaid@users.noreply.github.com>
2025-07-01 15:43:04 -0300

    Update receta.js

--- api/receta.js
+++ api/receta.js
@@ -19,7 +19,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "mistralai/mixtral-8x7b", // podés cambiar a mistralai/mixtral-8x7b si querés
+        model: "mistral/mistral-7b-instruct:free", // podés cambiar a mistralai/mixtral-8x7b si querés
         messages: [
           {
             role: "system",

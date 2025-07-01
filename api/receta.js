d51f3c16f3f94eb75f130e10fd11ce98b43b80bb
diablo0olbaid <104808307+diablo0olbaid@users.noreply.github.com>
2025-07-01 17:32:53 -0300

    Update receta.js

--- api/receta.js
+++ api/receta.js
@@ -1,10 +1,8 @@
 export default async function handler(req, res) {
-  // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-  // Manejar preflightt
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
